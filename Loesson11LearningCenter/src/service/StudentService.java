ee18512f2bb966f5f629c6a7ac6f89c125b4a9e5
Samandar-dev-hack <soatmurotovsamandar2327@gmail.com>
2025-05-20 12:14:04 -0700

    Teachersni qilayapman

---
+++ Loesson11LearningCenter/src/service/StudentService.java
@@ -0,0 +1,9 @@
+package service;
+
+public class StudentService {
+    public void service (){
+        while( true){
+
+        }
+    }
+}
---
+++ Loesson11LearningCenter/src/service/TeacherService.java
@@ -0,0 +1,35 @@
+package service;
+import entity.Group;
+
+import static db.DataSource.*;
+public class TeacherService {
+    public  void service(){
+        while(true){
+            System.out.println("""
+                    0 exit
+                    1 add students
+                    2 remove student
+                    3 attendance
+                    4 show attendance
+                    5 mark
+                    """);
+            switch (scanner.nextInt()){
+                case 0 ->{return;}
+                case 0 -> {
+                    addStudent();
+                }}
+                case 0 ->{}
+                case 0 ->{}
+                case 0 ->{}
+                case 0 ->{}
+            }
+            
+        }
+
+    private void addStudent() {
+        for (Group group : groups) {
+            if (group.)
+        }
+    }
+}
+}

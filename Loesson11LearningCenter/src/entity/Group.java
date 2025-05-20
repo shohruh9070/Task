8f89017152219e2fadd86c7158aa3fb10557b19d
SHOHRUH <shohruhmukhamedov6@gmail.com>
2025-05-21 00:12:21 +0500

    admin service user service qildim

--- Loesson11LearningCenter/src/entity/Group.java
+++ Loesson11LearningCenter/src/entity/Group.java
@@ -1,12 +1,68 @@
 package entity;
 
 import java.util.Set;
+import java.util.UUID;
 
 public class Group {
-    private String id;
+    private final String id=UUID.randomUUID().toString();
     private String name;
     private Integer maxLessonInMonth;
     private User teacher;
     private Set<User> students;
+    public Group(){
 
+    }
+    public Group(String name, Integer maxLessonInMonth, User teacher, Set<User> students) {
+        this.name = name;
+        this.maxLessonInMonth = maxLessonInMonth;
+        this.teacher = teacher;
+        this.students = students;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getMaxLessonInMonth() {
+        return maxLessonInMonth;
+    }
+
+    public void setMaxLessonInMonth(Integer maxLessonInMonth) {
+        this.maxLessonInMonth = maxLessonInMonth;
+    }
+
+    public User getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(User teacher) {
+        this.teacher = teacher;
+    }
+
+    public Set<User> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<User> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", maxLessonInMonth=" + maxLessonInMonth +
+                ", teacher=" + teacher +
+                ", students=" + students +
+                '}';
+    }
 }
